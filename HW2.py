7d817ab71d22c7e27bab15dbe3106f4598a10bff
Test1 <Test1>
2022-01-14 23:41:59 +0200

    3 rework of 3-d task

--- HW2.py
+++ HW2.py
@@ -13,9 +13,7 @@
 # переводит метры в мили, дюймы или ярды.
 
 
-
 # #1 TASK
-
 
 
 # num1 = int(input("Enter yor first number "))
@@ -31,12 +29,11 @@
 # 2 TASK
 
 
-
 #
-# num1 = int(input("Enter yor first number "))
-# num2 = int(input("Enter yor second number "))
-# num3 = int(input("Enter yor third number "))
-# opera = input(f"Please choose 'Biggest', 'Lowest','Arithmetic mean' ")
+# num1 = int(input("Enter you're first number "))
+# num2 = int(input("Enter you're second number "))
+# num3 = int(input("Enter you're third number "))
+# opera = input("Please choose 'Biggest', 'Lowest','Arithmetic mean' ")
 # if opera == 'Biggest':
 #     if num1 > num2 > num3:
 #         print(num1)
@@ -57,5 +54,20 @@
 #          print(num3)
 # elif opera == 'Arithmetic mean':
 #     print((num1 + num2 + num3) / 3)
+# else:
+#     print("Error")
+
+
+# Task 3
+
+#
+# num1 = int(input("Enter amount of meters you want to convert "))
+# opera = input("Please choose what you want to convert in: miles, inches, yards ")
+# if opera == 'miles':
+#     print(num1/1609)
+# elif opera == 'inches':
+#     print(num1/0.0254)
+# elif opera == 'yards':
+#     print((num1/0.9144))
 # else:
 #     print("Error")
