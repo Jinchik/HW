b72a4f8bc2d7e09beb8f62c60a1972ba0c53199b
Test1 <Test1>
2022-01-15 14:34:28 +0200

    Global rework2

---
+++ HW 4.py
@@ -0,0 +1,79 @@
+# Курс: «Введение в язык
+# программирования Python
+# Модуль 2. Операторы ветвлений
+# Часть 2
+# Задание 1
+# Пользователь вводит с клавиатуры номер дня недели
+# (1-7). Необходимо вывести на экран названия дня недели. Например, если 1, то на экране надпись понедельник,
+# 2 — вторник и т.д.
+# Задание 2
+# Пользователь вводит с клавиатуры номер месяца
+# (1-12). Необходимо вывести на экран название месяца.
+# Например, если 1, то на экране надпись январь, 2 — февраль и т.д.
+# Задание 3
+# Пользователь вводит с клавиатуры число. Если число
+# больше нуля нужно вывести надпись «Number is positive»,
+# если меньше нуля «Number is negative», если равно нулю
+# «Number is equal to zero»
+# Задание 4
+# Пользователь вводит два числа. Определить, равны
+# ли эти числа, и, если нет, вывести их на экран в порядке
+# возрастания
+
+#Task 4
+
+
+#
+# weekd = int(input('Please enter a number of the day of the week 1-7 '))
+# if weekd == 1:
+#     print("It is Monday, my friend :( ")
+# elif weekd == 2:
+#     print("It is Thuesday, my friend :\ ")
+# elif weekd == 3:
+#     print("It is Wednesday, my friend :/ ")
+# elif weekd == 4:
+#     print("It is Thursday, my friend :! ")
+# elif weekd == 5:
+#     print("It is Friday, my friend WEEEE ")
+# elif weekd == 6:
+#     print("It is Saturday, my friend :) ")
+# elif weekd == 7:
+#     print("It is Sunday, my friend :D ")
+# else:
+#     print("Error")
+
+
+#Task 5
+
+
+
+# month = int(input('Please enter a number of the month 1-12 '))
+# if month == 1:
+#     print("It is January, it's freezing mate brrr ")
+# elif month == 2:
+#     print("It is February, still freezing wow! ")
+# elif month == 3:
+#     print("It is March, flowers are growing from the snow :/ ")
+# elif month == 4:
+#     print("It is April, look at the trees :! ")
+# elif month == 5:
+#     print("It is May, everything are green, wonderful ")
+# elif month == 6:
+#     print("It is June, great weather, time to swim!  :) ")
+# elif month == 7:
+#     print("It is July, wow it's really hot, we need to hide somewhere in shadow ")
+# elif month == 8:
+#     print("It is August, still hot, really hot, where is water ")
+# elif month == 9:
+#     print("It is Septeeber, time for school, universities and work, boooring ")
+# elif month == 10:
+#     print("It is October, yellow is the best color ")
+# elif month == 11:
+#     print("It is November, New Year is near ")
+# elif month == 12:
+#     print("It is December, How, how, how here comes the Santa! ")
+# else:
+#     print("Error")
+
+
+#Task 6
--- HW3.py
+++ HW3.py
@@ -1,82 +1,73 @@
 # Курс: «Введение в язык
 # программирования Python
 # Модуль 2. Операторы ветвлений
-# Часть 2
+# Часть 1
 # Задание 1
-# Пользователь вводит с клавиатуры номер дня недели
-# (1-7). Необходимо вывести на экран названия дня недели. Например, если 1, то на экране надпись понедельник,
-# 2 — вторник и т.д.
+# Пользователь вводит с клавиатуры три числа. В зависимости от выбора пользователя программа выводит
+# на экран сумму трёх чисел или произведение трёх чисел.
 # Задание 2
-# Пользователь вводит с клавиатуры номер месяца
-# (1-12). Необходимо вывести на экран название месяца.
-# Например, если 1, то на экране надпись январь, 2 — февраль и т.д.
+# Пользователь вводит с клавиатуры три числа. В зависимости от выбора пользователя программа выводит
+# на экран максимум из трёх, минимум из трёх или среднеарифметическое трёх чисел.
 # Задание 3
-# Пользователь вводит с клавиатуры число. Если число
-# больше нуля нужно вывести надпись «Number is positive»,
-# если меньше нуля «Number is negative», если равно нулю
-# «Number is equal to zero»
-# Задание 4
-# Пользователь вводит два числа. Определить, равны
-# ли эти числа, и, если нет, вывести их на экран в порядке
-# возрастания
-
-#Task 4
+# Пользователь вводит с клавиатуры количество метров. В зависимости от выбора пользователя программа
+# переводит метры в мили, дюймы или ярды.
 
 
-#
-# weekd = int(input('Please enter a number of the day of the week 1-7 '))
-# if weekd == 1:
-#     print("It is Monday, my friend :( ")
-# elif weekd == 2:
-#     print("It is Thuesday, my friend :\ ")
-# elif weekd == 3:
-#     print("It is Wednesday, my friend :/ ")
-# elif weekd == 4:
-#     print("It is Thursday, my friend :! ")
-# elif weekd == 5:
-#     print("It is Friday, my friend WEEEE ")
-# elif weekd == 6:
-#     print("It is Saturday, my friend :) ")
-# elif weekd == 7:
-#     print("It is Sunday, my friend :D ")
-# else:
-#     print("Error")
+# #1 TASK
 
 
-#Task 5
+num1 = int(input("Enter yor first number "))
+num2 = int(input("Enter yor second number "))
+num3 = int(input("Enter yor third number "))
+oper = input("Please enter + or * ")
+if oper == "+":
+    print(num1 + num2 + num3)
+elif oper == "*":
+    print(num1 * num2 * num3)
+else:
+    print("Oops, you broke something")
+
+# # 2 TASK
 
 
-
-# month = int(input('Please enter a number of the month 1-12 '))
-# if month == 1:
-#     print("It is January, it's freezing mate brrr ")
-# elif month == 2:
-#     print("It is February, still freezing wow! ")
-# elif month == 3:
-#     print("It is March, flowers are growing from the snow :/ ")
-# elif month == 4:
-#     print("It is April, look at the trees :! ")
-# elif month == 5:
-#     print("It is May, everything are green, wonderful ")
-# elif month == 6:
-#     print("It is June, great weather, time to swim!  :) ")
-# elif month == 7:
-#     print("It is July, wow it's really hot, we need to hide somewhere in shadow ")
-# elif month == 8:
-#     print("It is August, still hot, really hot, where is water ")
-# elif month == 9:
-#     print("It is Septeeber, time for school, universities and work, boooring ")
-# elif month == 10:
-#     print("It is October, yellow is the best color ")
-# elif month == 11:
-#     print("It is November, New Year is near ")
-# elif month == 12:
-#     print("It is December, How, how, how here comes the Santa! ")
-# else:
-#     print("Error")
-
-
-#Task 6
+num1 = int(input("Enter you're first number "))
+num2 = int(input("Enter you're second number "))
+num3 = int(input("Enter you're third number "))
+opera = input("Please choose: You want the biggest number? Type b. Lowest? Type l. Arithmetic mean? Type a  ")
+if opera == 'b':
+    if num1 > num2 > num3:
+        print(num1)
+    elif num1 < num2 < num3:
+        print(num3)
+    elif num1 < num2 > num3:
+        print(num2)
+    elif num1 > num2 < num3:
+        print(num3)
+elif opera == 'l':
+    if num1 < num2 < num3:
+        print(num1)
+    elif num1 > num2 > num3:
+        print(num3)
+    elif num1 > num2 < num3:
+        print(num2)
+    elif num1 < num2 > num3:
+        print(num3)
+elif opera == 'a':
+    print((num1 + num2 + num3) / 3)
+else:
+    print("Oops, you broke something")
 
 
 
+# Task 3
+
+num1 = int(input("Enter amount of meters you want to convert "))
+opera = input("Please choose what you want to convert in: m for Miles,i for Inches,y for Yards ")
+if opera == 'm':
+    print(num1 / 1609)
+elif opera == 'i':
+    print(num1 / 0.0254)
+elif opera == 'y':
+    print((num1 / 0.9144))
+else:
+    print("Oops, you broke something")
