c1ca8567f2fa1a87a8e5435251646d318aa79017
Test1 <Test1>
2022-01-19 01:09:20 +0200

    New commit

--- MyTesting.py
+++ MyTesting.py
@@ -1,7 +1,9 @@
 num1 = int(input("Please neter a number "))
 num2 = int(input("Please neter a number "))
 num3 = int(input("Please neter a number "))
-if (num1 == 0, num2 == 0, num3 == 0):
-    print("0 not allowed")
-elif (num1 != 0, num2 != 0, num3 != 0):
-    print("Good to go, it isn't 0")
+opt = (input("Enter the sign "))
+if opt == "+":
+    print(f"{num1} + {num2} + {num3} ==" , num1+num2+num3)
+    print(num1+num2+num3)
+    # elif (num1 != 0, num2 != 0, num3 != 0):
+#     print("Good to go, it isn't 0")
