be591a0038f8b18343ebd5c2e347985dea2825a8
Test1 <Test1>
2022-01-31 23:32:57 +0200

    hw8 change

--- MyTesting.py
+++ MyTesting.py
@@ -1,9 +1,10 @@
-num1 = int(input("Please neter a number "))
-num2 = int(input("Please neter a number "))
-num3 = int(input("Please neter a number "))
-opt = (input("Enter the sign "))
+# num1 = int(input("Please neter a number "))
+# num2 = int(input("Please neter a number "))
+# num3 = int(input("Please neter a number "))
+# opt = (input("Enter the sign "))
 if opt == "+":
     print(f"{num1} + {num2} + {num3} ==" , num1+num2+num3)
     print(num1+num2+num3)
-    # elif (num1 != 0, num2 != 0, num3 != 0):
-#     print("Good to go, it isn't 0")
+    elif (num1 != 0, num2 != 0, num3 != 0):
+    print("Good to go, it isn't 0")
+
