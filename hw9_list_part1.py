d4745baf685df9fb06a94c8f0b8597b66877c8bf
Test1 <Test1>
2022-02-10 23:42:01 +0200

    Lot of HW3

--- hw9_list_part1.py
+++ hw9_list_part1.py
@@ -7,25 +7,21 @@
 # упала на лапу Азора; доход; А буду я у дуба.
 
 
+text = input("Please, enter the text ")
+if text != text[::-1]:
+    print("It is not palindrome")
+else:
+    print("It is palindrome")
 
 
-# text = input("Please, enter the text ")
-# if text != text[::-1]:
-#     print("It is not palindrome")
-# else:
-#     print("It is palindrome")
-#
-#
-#
-# text = input("Please, enter the text ")
-# for i in range(len(text)):
-#     if text[i] != text[-i-1]:
-#         print("It is not palindrome")
-#         break
-# else:
-#     print("It is palindrome")
 
-
+text = input("Please, enter the text ")
+for i in range(len(text)):
+    if text[i] != text[-i-1]:
+        print("It is not palindrome")
+        break
+else:
+    print("It is palindrome")
 
 
 #
@@ -37,92 +33,27 @@
 # экран измененный текст.
 
 
-mytext = "Python is the best language!"
-mytext_temp = mytext.lower()
-listold = list(mytext.split())
-listnew = list(mytext_temp.split())
-word = ["Python", "is", "best"]
-for i in range(len(listnew)) :
-    print(listnew[i])
-    for j in word :
-
-        if j in listnew[i] :
-            listold[i] = listnew[i].upper()
-print(' '.join(listold))
+text = "Python is the best language!"
+word_list = ["Python", "is", "best"]
+for word in word_list:
+    if word in text:
+        text = text.replace(word, word.upper())
+print(text)
 
 
-
-# s = '1chairghdcdfchaircdchfchair'
-# symbol = 'chair'
-# new_symbol = '*CHAIR*'
-# first_symbol = symbol[0]
-# new_line = ''
-# i = 0
-# while i != len(s):
-#     if s[i] == first_symbol:
-#         if symbol == s[i:i + len(symbol)]:
-#             new_line = new_line + new_symbol
-#             i = i + len(symbol)
-#         else:
-#             new_line += s[i]
-#             i += 1
-#     else:
-#         new_line += s[i]
-#         i += 1
-# print(new_line)
-
-
-
-
-# #Вариант работает при точном совпадении регистра
-#
-# text = '''The Star Wars franchise depicts the adventures of characters "A long time ago in a galaxy far, far away",[5] in which humans and many species of aliens (often humanoid) co-exist with robots (typically referred to in the films as 'droids'), who may assist them in their daily routines; space travel between planets is common due to lightspeed hyperspace technology.[6][7][8] Spacecraft range from small starfighters, to huge capital ships such as the Star Destroyers, to space stations such as the moon-sized Death Stars. Telecommunication includes two-way audio and audiovisual screens, and holographic projections.
-#
-# A mystical power known as the Force is described in the original film as "an energy field created by all living things ... [that] binds the galaxy together".[9] Through training and meditation, those whom "the Force is strong with" are able to perform various superpowers (such as telekinesis, precognition, telepathy, and manipulation of physical energy).[10] The Force is wielded by two major knightly orders at conflict with each other: the Jedi, peacekeepers of the Galactic Republic who act on the light side of the Force through non-attachment and arbitration, and the Sith, who use the dark side by manipulating fear and aggression. While Jedi Knights can be numerous, the Dark Lords of the Sith (or 'Darths') are intended to be limited to two: a master and their apprentice.[11]
-#
-# Force-wielders are very limited in numbers in comparison to the population. The Jedi and Sith prefer the use of a weapon called a lightsaber, a blade of energy that can cut through virtually any surface and deflect energy bolts. The rest of the population, as well as renegades and soldiers, use laser-powered blaster firearms. In the outer reaches of the galaxy, crime syndicates such as the Hutt cartel are dominant. Bounty hunters are often employed by both gangsters and governments. Illicit activities include smuggling and slavery.'''
-# word_list = ['Star','Wars','Force','slavery']
-# for word in word_list:
-# 	if word in text:
-# 		text = text.replace(word,word.upper())
-# print(text)
-#
-# #Не чувствителен к регистру, но обратите внимание, что воздействует только на "чистые слова", самое последнее слова, является один целым с точкой, потому его не меняет
-# text = '''The Star Wars franchise depicts the adventures of characters "A long time ago in a galaxy far, far away",[5] in which humans and many species of aliens (often humanoid) co-exist with robots (typically referred to in the films as 'droids'), who may assist them in their daily routines; space travel between planets is common due to lightspeed hyperspace technology.[6][7][8] Spacecraft range from small starfighters, to huge capital ships such as the Star Destroyers, to space stations such as the moon-sized Death Stars. Telecommunication includes two-way audio and audiovisual screens, and holographic projections.
-#
-# A mystical power known as the Force is described in the original film as "an energy field created by all living things ... [that] binds the galaxy together".[9] Through training and meditation, those whom "the Force is strong with" are able to perform various superpowers (such as telekinesis, precognition, telepathy, and manipulation of physical energy).[10] The Force is wielded by two major knightly orders at conflict with each other: the Jedi, peacekeepers of the Galactic Republic who act on the light side of the Force through non-attachment and arbitration, and the Sith, who use the dark side by manipulating fear and aggression. While Jedi Knights can be numerous, the Dark Lords of the Sith (or 'Darths') are intended to be limited to two: a master and their apprentice.[11]
-#
-# Force-wielders are very limited in numbers in comparison to the population. The Jedi and Sith prefer the use of a weapon called a lightsaber, a blade of energy that can cut through virtually any surface and deflect energy bolts. The rest of the population, as well as renegades and soldiers, use laser-powered blaster firearms. In the outer reaches of the galaxy, crime syndicates such as the Hutt cartel are dominant. Bounty hunters are often employed by both gangsters and governments. Illicit activities include smuggling and slavery.'''
-# word_list = ['Star','Wars','Force','slavery']
-# text_list = text.split(' ')
-# for i in range(len(word_list)):
-# 		for j in range(len(text_list)):
-# 				if word_list[i].lower() == text_list[j].lower():
-# 						text_list[j] = text_list[j].upper()
-# upper_text = ''
-# for text_word in text_list:
-# 		upper_text = upper_text + ' ' + text_word
-# print(upper_text)
-# 2.0#Не чувствителен к регистру, но обратите внимание, что воздействует только на "чистые слова", самое последнее слова, является один целым с точкой, потому его не меняет
-text = '''The Star Wars franchise depicts the adventures of characters "A long time ago in a galaxy far, far away",[5] in which humans and many species of aliens (often humanoid) co-exist with robots (typically referred to in the films as 'droids'), who may assist them in their daily routines; space travel between planets is common due to lightspeed hyperspace technology.[6][7][8] Spacecraft range from small starfighters, to huge capital ships such as the Star Destroyers, to space stations such as the moon-sized Death Stars. Telecommunication includes two-way audio and audiovisual screens, and holographic projections.
-
-A mystical power known as the Force is described in the original film as "an energy field created by all living things ... [that] binds the galaxy together".[9] Through training and meditation, those whom "the Force is strong with" are able to perform various superpowers (such as telekinesis, precognition, telepathy, and manipulation of physical energy).[10] The Force is wielded by two major knightly orders at conflict with each other: the Jedi, peacekeepers of the Galactic Republic who act on the light side of the Force through non-attachment and arbitration, and the Sith, who use the dark side by manipulating fear and aggression. While Jedi Knights can be numerous, the Dark Lords of the Sith (or 'Darths') are intended to be limited to two: a master and their apprentice.[11]
-
-Force-wielders are very limited in numbers in comparison to the population. The Jedi and Sith prefer the use of a weapon called a lightsaber, a blade of energy that can cut through virtually any surface and deflect energy bolts. The rest of the population, as well as renegades and soldiers, use laser-powered blaster firearms. In the outer reaches of the galaxy, crime syndicates such as the Hutt cartel are dominant. Bounty hunters are often employed by both gangsters and governments. Illicit activities include smuggling and slavery.'''
-word_list = ['Star','Wars','Force','slavery']
+text ='Python is the best language!'
+word_list = ["Python", "is", "best"]
 text_list = text.split(' ')
 for i in range(len(word_list)):
-		for j in range(len(text_list)):
-				if word_list[i].lower() in text_list[j].lower():
-						text_list[j] = text_list[j].upper()
+    for j in range(len(text_list)):
+        if word_list[i].lower() in text_list[j].lower():
+            text_list[j] = text_list[j].upper()
 upper_text = ''
 for text_word in text_list:
-		upper_text = upper_text + ' ' + text_word
+    upper_text = upper_text + ' ' + text_word
 print(upper_text)
 
-# 'slavery' == 'slavery.' 1.0 FALSE
-# 'slavery' in 'slavery.' 2.0 TRUE
-# 'slavery' in 'slavery' 2.0 TRUE
+
 
 
 
@@ -135,6 +66,5 @@ print(upper_text)
 # результат.
 
 
-
-# text = 'Is something is here? Really Something is here. Belive me! '
-# print(text.count('. ')+text.count('! ')+text.count('? '))
+text = 'Is something is here? Really Something is here. Belive me! '
+print(text.count('. ')+text.count('! ')+text.count('? '))
--- hw9_list_part3.py
+++ hw9_list_part3.py
@@ -12,109 +12,47 @@
 # ■ Сформировать третий список, содержащий только
 # минимальное и максимальное значение каждого из
 # списков.
-list1 = [1,7,23,88,666,4,3,2,0,1,5]
-list2 = [19,12,23,8,686,3,3,2,0,9,5]
-lisplus=[]
-liswithoutsame =[]
-
-
-
-
-
+list1 = [1, 7, 23, 88, 666, 4, 3, 2, 0, 1, 5,-1]
+list2 = [19, 12, 23, 8, 686, 3, 3, 2, 0, 9, 5,-2]
+lisplus = []
+liswithoutsame = []
+lisonlysame = []
+notsame = []
+mini = []
+maxi = []
+temp1 =1
+temp2 =0
+# #1 way
 # listplus =[list1]+[list2]#2 in 1
 # print(listplus)
 # liswithoutsame = list(set(listplus[0]))
-# print(liswithoutsame)
-
-
-
-
+# print(liswithoutsame)# without repeat
+# 2 way
 for num1 in list1:
     lisplus.append(num1)
 for num2 in list2:
     lisplus.append(num2)
-# print(lisplus)   #2 in 1
-for number in range (len(lisplus)):
-
-    if lisplus[number] != lisplus[number+1] :
-
+print(lisplus)  # 2 in 1
+for number in range(1, len(lisplus)):
+    if lisplus[number] != lisplus[number - 1]:
         liswithoutsame.append(lisplus[number])
-        print(liswithoutsame)
+print('without repeat', liswithoutsame)  # without repeat
+for i in range(len(list1)):
+    for j in range(len(list2)):
+        if list1[i] == list2[j]:
+            lisonlysame.append(list1[i])
+print("lisonlysame", lisonlysame)  # same
+for i in lisplus:
+    if i not in notsame:
+        notsame.append(i)
+print("notsame", notsame)  # unic
+for k in lisplus:
+    if k < temp1:
+        temp1 = k
+    elif k > temp2:
+        temp2 = k
+mini.append(temp1)
+maxi.append(temp2)
+print("Max",maxi,"min",mini)
 
 
-
-#############
-
-# l = [1,2,3,4,4,5,6,7,8,9,0,1,3]
-# for i in range(1,len(l)):
-#     if l[i] == l[i-1]:
-#         l[i] = ''
-# while '' in l:
-#     l.remove('')
-# print(l)
-# l = [1,2,3,4,4,5,6,7,8,9,0,1,3]
-# a = 1
-# length = len(l)
-# while a < length:
-#     if l[a] == l[a-1]:
-#         l.pop(a)
-#     a = a + 1
-#     length = len(l)
-# print(l)
-
-
-
-###############
-
-
-
-# s1 = [4,1,2,3,4]
-# s2 = [3,10,2,5,7]
-# mini = s1[0]
-# maxi = s1[0]
-# s_minmax = []
-# for n in range(len(s1)):
-#     if mini > s1[n]:
-#         mini = s1[n]
-#         s_minmax.append(mini)
-#     if maxi < s1[n]:
-#         maxi = s1[n]
-#         s_minmax.append(maxi)
-# print(mini, maxi)
-# mini = s2[0]
-# maxi = s2[0]
-# for n in range(len(s2)):
-#     if mini > s2[n]:
-#         mini = s2[n]
-#         s_minmax.append(mini)
-#     if maxi < s2[n]:
-#         maxi = s2[n]
-#         s_minmax.append(maxi)
-# print(s_minmax)
-#
-# '''# ■ Сформировать третий список, содержащий только уникальные элементы каждого из списков;
-# # ■ Сформировать третий список, содержащий только минимальное и максимальное значение каждого из списков.'''
-# s1 = [1,2,3,4]
-# s2 = [1,2,5,6,8]
-# s3 = s1[:]
-# for s in s2:
-#     if s not in s3:
-#         s3.append(s)
-#
-# s1 = [1,2,3,4]
-# s2 = [1,2,5,6,8]
-# s3 = s1[:]
-# for s in s2:
-#     if s in s3:
-#         continue
-#     else:
-#         s3.append(s)
-#
-#
-# s1 = [1,2,3,4]
-# s2 = [1,2,5,6,8]
-# s_rr = s1[:]
-# for i in s2:
-#     if i not in s_rr:
-#         s_rr.append(i)
-# print(s_rr)
