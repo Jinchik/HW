5e1b9e922837ffcbfddd6b627646c51937a809dc
Test1 <Test1>
2022-02-22 19:01:54 +0200

    Mytesting

--- MyTesting.py
+++ MyTesting.py
@@ -53,3 +53,149 @@
 # print(my_list)
 
 
+
+
+# def eazy_num(list):
+#     n = list[0]
+#     counter = 0
+#     for i in range(1, n + 1):
+#         if n % i == 0:
+#             counter += 1
+#         return 'Easy number' if counter == 2 else 'Not Easy number'
+#
+#
+# eazy_num([7, 2, 6, 8, 11])
+
+
+
+def simple(number):
+    for some_number in range(2,number):
+        if number%some_number == 0:
+            return False
+    return True
+
+def simple_list(list_of_numbers):
+    new_list = []
+    for number in list_of_numbers:
+        if simple(number):
+            new_list.append(number)
+    l = len(new_list)
+    return l
+
+
+random_list = [1,4,5,7,3,5,6,8,137,42,65]
+print(simple_list(random_list))
+
+
+
+
+
+
+def simple_numbers_count_m1(numbers):
+    count = 0
+    for number in numbers:
+        number = str(number)
+        if number.lstrip("-").isnumeric():
+            number = int(number)
+            if number >= 2:
+                simple = True
+                for i in range(2, number):
+                    if number % i == 0:
+                        simple = False
+                        break
+                if simple:
+                    count = count + 1
+            else:
+                continue
+        else:
+            return "INCORRECT ENTRY!"
+    return count
+
+
+some_numbers = [2, 42, 137, 1, 3, -2]
+print(simple_numbers_count_m1(some_numbers))
+print()
+
+
+
+
+
+
+
+
+
+s = '-23+12'
+number1 = ''
+number2 = ''
+symb = ['+', '-', '*', '/']
+
+for i in range(len(s)):
+    n = s[i]
+    for j in symb:
+        if n == j:
+            number1 = s[:i]
+            number2 = s[i + 1:]
+            mark = n
+if mark == '+':
+    print(number1, '+', number2, '=', int(number1) + int(number2))
+elif mark == '-':
+    print(number1, '-', number2, '=', int(number1) - int(number2))
+elif mark == '*':
+    print(number1, '*', number2, '=', int(number1) * int(number2))
+elif mark == '/':
+    print(number1, '/', number2, '=', int(number1) / int(number2))
+
+
+
+
+#Variables
+a = str(input("Input some impression: "))
+operators = ['+', '-', '*', '/']
+operator = 0
+operator_index = 0
+# Cycle for enumerating type of operator
+for i in a:
+    for b in operators:
+        if b == i:
+            operator = b
+            break
+# Enumerating operator's index
+operator_index = a.index(operator)
+# Calculating and printing results
+if operator == "+":
+    print(int(a[:operator_index]) + int(a[operator_index+1:]))
+elif operator == "-":
+    print(int(a[:operator_index]) - int(a[operator_index+1:]))
+elif operator == "*":
+    print(int(a[:operator_index]) * int(a[operator_index+1:]))
+elif operator == "/":
+    print(int(a[:operator_index]) / int(a[operator_index+1:]))
+else:
+    print("You input something wrong! ")
+
+
+
+
+
+ numbers = input('Enter the operation (possible +, -, *, / ): ')
+# operator1 = '+'
+# operator2 = '-'
+# operator3 = '*'
+# operator4 = '/'
+# if operator1 in numbers:
+#     a, b = numbers.split('+')
+#     print(int(a) + int(b))
+# elif operator2 in numbers:
+#     a,b = numbers.split('-')
+#     print(int(a) - int(b))
+# elif operator3 in numbers:
+#     a,b = numbers.split('*')
+#     print(int(a) * int(b))
+# elif operator4 in numbers:
+#     a,b = numbers.split('/')
+#     print(int(a) / int(b))
+# else:
+#     print("Uncorrected operation")
+
+
+
