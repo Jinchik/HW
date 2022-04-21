6e5abba1f0c3cb1c325e43adc33f958adbef9433
Test1 <Test1>
2022-04-21 23:36:54 +0300

    Pizdec hard work HW 11

--- HW_11_function.py
+++ HW_11_function.py
@@ -11,22 +11,22 @@
 # Напишите функцию, вычисляющую произведение
 # элементов списка целых. Список передаётся в качестве параметра. Полученный результат возвращается из функции.
 
-# def multifunc(x):
-#     multi = 1
-#     for num in x:
-#         multi = multi * num
-#     return multi
-#     print(multi)
-#
-#
-# a = []
-# while True:
-#     b = int(input(f"Please enter numbers to see multi of all number. Enter 0 to stop. \n"))
-#     if b == 0:
-#         break
-#     else:
-#         a.append(b)
-# print(multifunc(a))
+def multifunc(x):
+    multi = 1
+    for num in x:
+        multi = multi * num
+    return multi
+    print(multi)
+
+
+a = []
+while True:
+    b = int(input(f"Please enter numbers to see multi of all number. Enter 0 to stop. \n"))
+    if b == 0:
+        break
+    else:
+        a.append(b)
+print(multifunc(a))
 
 
 # Задание 2
@@ -35,55 +35,50 @@
 # Полученный результат возвращается из функции.
 
 
-# def min(x):
-#     min = 0
-#     for n in x:
-#         if min < n:
-#             min = n
-#
-#     return min
-#
-#
-# a = []
-# while True:
-#     b = int(input(f"Please enter numbers to see lowest number. Enter 0 to stop. \n"))
-#     if b == 0:
-#         break
-#     else:
-#         a.append(b)
-# print(min(a))
+def min(x):
+    min = 0
+    for n in x:
+        if min > n:
+            min = n
+
+    return min
+
+
+a = []
+while True:
+    b = int(input(f"Please enter numbers to see lowest number. Enter 0 to stop. \n"))
+    if b == 0:
+        break
+    else:
+        a.append(b)
+print(min(a))
 
 # Задание 3
 # Напишите функцию, определяющую количество простых чисел в списке целых. Список передаётся в качестве
 # параметра. Полученный результат возвращается из функции.
 
 
-
-# def simplecount(x):
-#     simple = []
-#     d = 2
-#     for n in x:
-#         while n % d != 0:
-#             d += 1
-#             simple.append(n)
-#         if n == 0:
-#             break
-#
-#     return simple
-#
-# a = []
-# while True:
-#     b = int(input(f"Please enter numbers to see lowest number. Enter 0 to stop. \n"))
-#     if b == 0:
-#         break
-#     else:
-#         a.append(b)
-# print(simplecount(a))
+def simple_numb(n):
+    numbers = []
+    count = (0)
+    for num in n:
+        if num % 2 != 0:
+            count += 1
+            numbers.append(num)
+    return (f'count = {count},numbers = {numbers}')
 
 
-
-
-
+n = []
+while True:
+    b = int(input(f"Please enter numbers from 2 and higher then 2 to se the list of simple numbers. Enter 0 to stop. \n"))
+    if b == 0:
+        break
+    elif b == 1:
+        print("You had to enter number higher then 2 or 2")
+        quit()
+    else:
+        n.append(b)
+print(simple_numb(n))
 
 # Задание 4
 # Напишите функцию, удаляющую из списка целых
@@ -91,10 +86,57 @@
 # количество удаленных элементов.
 
 
+def deleting(x):
+    dellist = []
+    print(f'It is full list = {x}')
+    count = 0
+    for i in x:
+        dellist.append(i)
+    for dellist[0] in dellist:
+        print(f'{dellist[1]} - This number will be deleted')
+        del dellist[1]
+        count += 1
+    return (f'count of deleted numbers = {count}, Numbers that left = {dellist}')
+a = []
+while True:
+    b = int(input(f"Please enter numbers to fill the list. Enter 0 to stop. \n"))
+    if b == 0:
+        break
+    else:
+        a.append(b)
+print(deleting(a))
+
+
+
+
+
+
 # Задание 5
 # Напишите функцию, которая получает два списка в
 # качестве параметра и возвращает список, содержащий
 # элементы обоих списков.
+
+def twolist(x,y):
+    together = []
+
+    for i in x,y:
+        for j in i:
+            together.append(j)
+    return together
+
+
+a = []
+b = a
+while True:
+    c = int(input(f"Please enter numbers to make 1 list from 2. Enter 0 to stop. \n"))
+    if c == 0:
+        break
+    else:
+        a.append(c)
+print(twolist(a,b))
+
+
+
 
 
 # Задание 6
@@ -102,3 +144,20 @@
 # элемента списка целых. Значение для степени передаётся
 # в качестве параметра, список тоже передаётся в качестве
 # параметра. Функция возвращает новый список, содержащий полученные результаты
+
+def degree(x,y):
+    degreelist = []
+    for i in x:
+        d=i**y
+        degreelist.append(d)
+    return degreelist
+a = []
+b = int(input("Enter please the degree number, you want to high in. \n"))
+while True:
+    c = int(input(f"Please enter numbers to make the list. Enter 0 to stop. \n"))
+    if c == 0:
+        break
+    else:
+        a.append(c)
+print(degree(a,b))
+
