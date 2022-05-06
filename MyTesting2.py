59392cdc183d4026164c1cc7a492d69a9a804a5b
Test1 <Test1>
2022-05-06 20:43:30 +0300

    Final exam 3 patch

--- MyTesting2.py
+++ MyTesting2.py
@@ -176,20 +176,26 @@
 # x = Fraction(Fraction.numerator1,Fraction.denominator1)
 # y = Fraction(Fraction.numerator2,Fraction.denominator2)
 # print(x+y)
-import pygame
+# import pygame
+#
+# run = True
+# width = 400
+# height = 100
+# pygame.init()
+# screen = pygame.display.set_mode((width, height))
+# font = pygame.font.SysFont(None, 48)
+# text = font.render("Welcome to pygame", True, (255, 255, 255))
+# screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+# pygame.display.flip()
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT\
+#         or event.type == pygame.MOUSEBUTTONUP\
+#         or event.type == pygame.KEYUP:
+#             run = False
 
-run = True
-width = 400
-height = 100
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-font = pygame.font.SysFont(None, 48)
-text = font.render("Welcome to pygame", True, (255, 255, 255))
-screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
-pygame.display.flip()
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT\
-        or event.type == pygame.MOUSEBUTTONUP\
-        or event.type == pygame.KEYUP:
-            run = False
+
+a = 15
+print(type(a))
+if type(a) != str:
+    print("hello")
--- The_final_exam.py
+++ The_final_exam.py
@@ -3,29 +3,29 @@
 # прямоугольном треугольнике и выводит его площадь. Каждое число
 # записано в отдельной строке.
 
-def triangle_square(a, b):
-    try:
-        while a != "0" and b != "0":
-            if a != 0 and b != 0 and a.isdigit() and b.isdigit():
-                a = int(a)
-                b = int(b)
-                print(a * b / 2)
-                a = input("Please enter the  1 leg or 0 to break ")
-                b = input("Please enter the  2 leg or 0 to break ")
-
-            elif a.isalpha() or b.isalpha():
-                print("You entered the letter")
-                break
-    except:
-        print("oops you broke something")
-
-
-try:
-    a = input("Please enter the  1 leg or 0 to break ")
-    b = input("Please enter the  2 leg or 0 to break ")
-    print(triangle_square(a, b))
-except ValueError:
-    print("You wrote a wrong number o letter")
+# def triangle_square(a, b):
+#     try:
+#         while a != "0" and b != "0":
+#             if a != 0 and b != 0 and a.isdigit() and b.isdigit():
+#                 a = int(a)
+#                 b = int(b)
+#                 print(a * b / 2)
+#                 a = input("Please enter the  1 leg or 0 to break ")
+#                 b = input("Please enter the  2 leg or 0 to break ")
+#
+#             elif a.isalpha() or b.isalpha():
+#                 print("You entered the letter")
+#                 break
+#     except:
+#         print("oops you broke something")
+#
+#
+# try:
+#     a = input("Please enter the  1 leg or 0 to break ")
+#     b = input("Please enter the  2 leg or 0 to break ")
+#     print(triangle_square(a, b))
+# except ValueError:
+#     print("You wrote a wrong number o letter")
 
 # 2. В школе решили набрать три новых математических класса. Так как
 # занятия по математике у них проходят в одно и то же время, было
@@ -37,51 +37,201 @@ except ValueError:
 # учащихся в каждом из трех классов.
 
 
-def amount_of_students(a,b,c):
-    try:
-        while a != "0" and b != "0" and c != "0":
-            if a != 0 and b != 0 and c != 0 and a.isdigit() and b.isdigit() and c.isdigit():
-                a = int(a)
-                b = int(b)
-                c = int(c)
-                print ((a+b+c) // 2)
-                a = input("Please enter the amount of students in 1 grade ")
-                b = input("Please enter the amount of students in 2 grade ")
-                c = input("Please enter the amount of students in 3 grade ")
-
-            elif a.isalpha() or b.isalpha() or c.isalpha() :
-                print("You entered the letter or 0")
-                break
-    except:
-        print("oops you broke something")
-try:
-    a = input("Please enter the amount of students in 1 grade ")
-    b = input("Please enter the amount of students in 2 grade ")
-    c = input("Please enter the amount of students in 3 grade ")
-    print(amount_of_students(a,b,c))
-except ValueError:
-    print("You wrote a wrong number o letter")
-
-
-
+# def amount_of_students(a,b,c):
+#     try:
+#         while a != "0" and b != "0" and c != "0":
+#             if a != 0 and b != 0 and c != 0 and a.isdigit() and b.isdigit() and c.isdigit():
+#                 a = int(a)
+#                 b = int(b)
+#                 c = int(c)
+#                 print ((a+b+c) // 2)
+#                 a = input("Please enter the amount of students in 1 grade ")
+#                 b = input("Please enter the amount of students in 2 grade ")
+#                 c = input("Please enter the amount of students in 3 grade ")
+#
+#             elif a.isalpha() or b.isalpha() or c.isalpha() :
+#                 print("You entered the letter or 0")
+#                 break
+#     except:
+#         print("oops you broke something")
+# try:
+#     a = input("Please enter the amount of students in 1 grade ")
+#     b = input("Please enter the amount of students in 2 grade ")
+#     c = input("Please enter the amount of students in 3 grade ")
+#     print(amount_of_students(a,b,c))
+# except ValueError:
+#     print("You wrote a wrong number o letter")
+#
 
 
 # 3. Даны три целых числа. Определите, сколько среди них совпадающих.
 # Программа должна вывести одно из чисел: 3 (если все совпадают), 2
 # (если два совпадает) или 0 (если все числа различны).
+
+# def same_numb(a, b, c):
+#     try:
+#         while type(a) != str or type(b) != str or type(c) != str or a != str(" ") or b != str(" ") or c != str(" "):
+#             same = 0
+#             if a == b and b == c:
+#                 same += 3
+#                 print(f"All numbers are equal {same}")
+#                 same = 0
+#                 a = int(input("Please enter the 1 number or s to end "))
+#                 b = int(input("Please enter the 2 number or s to end "))
+#                 c = int(input("Please enter the 3 number or s to end "))
+#
+#             elif a == b or a == c or b == c:
+#                 same += 2
+#                 print(f"Two numbers are equal {same}")
+#                 same = 0
+#                 a = int(input("Please enter the 1 number or s to end "))
+#                 b = int(input("Please enter the 2 number or s to end "))
+#                 c = int(input("Please enter the 3 number or s to end "))
+#             else:
+#                 same = 0
+#                 print(f"No equal numbers {same}")
+#                 a = int(input("Please enter the 1 number or s to end "))
+#                 b = int(input("Please enter the 2 number or s to end "))
+#                 c = int(input("Please enter the 3 number or s to end "))
+#     except ValueError:
+#         return "You wrote letter or entered '_'!"
+#     except:
+#         return "You broke something"
+#
+#
+# try:
+#     a = int(input("Please enter the 1 number or s to end "))
+#     b = int(input("Please enter the 2 number or s to end "))
+#     c = int(input("Please enter the 3 number or s to end "))
+#     print(same_numb(a, b, c))
+#
+# except ValueError:
+#     print("You wrote letter or entered '_'!")
+# except:
+#     print("You broke something")
+
 # 4. Дано натуральное число. Требуется определить, является ли год с
 # данным номером високосным. Если год является високосным, то
 # выведите YES, иначе выведите NO. Напомним, что в соответствии с
 # григорианским календарем, год является високосным, если его номер
 # кратен 4, но не кратен 100, а также если он кратен 400.
+
+# def leap_year(a):
+#     try:
+#         while type(a) != str or a != str(" "):
+#
+#             if a % 4 == 0 and a % 100 > 0 or a % 400 == 0:
+#                 print(f"Yes it is a Leap year! {a}! By the way, if you want to stop, just print any letter...")
+#                 a = int(input("Please enter the Year: "))
+#             else:
+#
+#                 print(f"No it isn't a Leap year! {a}! By the way, if you want to stop, just print any letter...")
+#                 a = int(input("Please enter the Year: "))
+#
+#     except ValueError:
+#         return "You wrote letter or entered '_'!"
+#     except:
+#         return "You broke something"
+#
+#
+# try:
+#     a = int(input("Please enter the Year: "))
+#     print(leap_year(a))
+#
+# except ValueError:
+#     print("You wrote letter or entered '_'!")
+# except:
+#     print("You broke something")
+
+
 # 5. Дано положительное действительное число X. Выведите его первую
 # цифру после десятичной точки.
+
+# def numb_after_dot(a):
+#     try:
+#         while str(float(a)) != str or str(a) != str(" "):
+#             a = str(int(a*10))[-1]
+#             print(a)
+#             a = float(input("Please enter the number to know the first number after the . : "))
+#
+#
+#     except ValueError:
+#         return "You wrote letter or entered '_'!"
+#     except:
+#         return "You broke something"
+#
+#
+# try:
+#     a = float(input("Please enter the number to know the first number after the . : "))
+#     print(numb_after_dot(a))
+#
+# except ValueError:
+#     print("You wrote letter or entered '_'!")
+# except:
+#     print("You broke something")
+
+
 # 6. Факториалом числа n называется произведение 1 × 2 × ... × n.
 # Обозначение: n!.
 # По данному натуральному n вычислите значение n!. Пользоваться
 # математической библиотекой math в этой задаче запрещено.
+
+# try:
+#     def facto(a):
+#         f = int(1)
+#         x = int(a[:])
+#         while x != int(0) or str(float(a)) != a.isalpha() or str(a) != str(" "):
+#             x = int(a[:])
+#             for i in range(1, int(a) + 1):
+#                 f = int(i) * int(f)
+#                 x -= 1
+#                 if x == 0:
+#                     print(f)
+#                     f = 1
+#                     break
+#             a = input("Please enter the number to know it's factorial, or any letter ot '_' to break . : ")
+#
+# except ValueError:
+#     print("You wrote letter or entered '_'!")
+# except:
+#     print("You broke something")
+#
+#
+# try:
+#     a = input("Please enter the number to know it's factorial, or any letter ot '_' to break . : ")
+#     print(facto(a))
+# except ValueError:
+#     print("You wrote letter or entered '_'!")
+# except:
+#     print("You broke something")
+
 # 7. Дано два числа a и b. Выведите гипотенузу треугольника с заданными
 # катетами.
+
+try:
+    def hypotenuse(x, y):
+        z = float()
+        while (x, y) != int(0) or str(float(x, y)) != (x, y).isalpha() or str(x, y) != str(" "):
+            z = (float(x) ** 2 + float(y) ** 2)**0.5
+            print(float(z))
+            x = input("Please enter the number the 1 Cathetus or any letter ot '_' to break:  ")
+            y = input("Please enter the number the 2 Cathetus or any letter ot '_' to break:  ")
+
+
+except ValueError:
+    print("You wrote letter or entered '_'!")
+except:
+    print("You broke something")
+
+try:
+    x = input("Please enter the number the 1 Cathetus or any letter ot '_' to break:  ")
+    y = input("Please enter the number the 2 Cathetus or any letter ot '_' to break:  ")
+    print(hypotenuse(x,y))
+except ValueError:
+    print("You wrote letter or entered '_'!")
+except:
+    print("You broke something")
+
 # 8. Дано N чисел: сначала вводится число N, затем вводится ровно N целых
 # чисел. Подсчитайте количество нулей среди введенных чисел и
 # выведите это количество. Вам нужно подсчитать количество чисел,
